43db76bcf7a15cf49e3fe8e2c675512ffcae5876
Kevin Santana <kevin_santana.araujo@hotmail.com>
2021-09-20 14:07:13 -0300

    testes unitarios

---
+++ tests/integration/database/conftest.py
@@ -0,0 +1,26 @@
+# import pytest
+
+# from cashback_api.database import DataBase
+
+
+# class DatabaseTest(DataBase):
+#     def dict(self):
+#         return {}
+
+
+# @pytest.fixture(scope='module')
+# def setup_module():
+#     pass
+
+
+# @pytest.fixture(scope='module')
+# def teardown_module():
+#     db = DatabaseTest()
+#     db.query_string = "DELETE FROM REVENDEDOR WHERE REVENDEDOR.CPF = '82121823042'"
+#     db.insert()
+
+
+
+# @pytest.mark.usefixtures("teardown_module")
+# def test_tear_down():
+#     pass
--- tests/unit/utils/test_jwt_token.py
+++ tests/unit/utils/test_jwt_token.py
@@ -2,15 +2,15 @@ from tests.unit.utils import client
 
 
 def test_http_bearer_nao_autenticado_deve_retornar_403():
-    response = client.post("/v1/revendedor/", headers={"Authorization": "Not a cool auth"})
+    response = client.post("/v1/compras/", headers={"Authorization": "Not a cool auth"})
     assert response.json().get("status") == 403
 
 
 def test_http_bearer_token_invalido_deve_retornar_401():
-    response = client.post("/v1/revendedor/", headers={"Authorization": "Bearer isso nao e uma autenticacao"})
+    response = client.post("/v1/compras/", headers={"Authorization": "Bearer isso nao e uma autenticacao"})
     assert response.json().get("status") == 401
 
 
 def test_http_bearer_token_nao_informado_deve_retornar_403():
-    response = client.post("/v1/revendedor/", headers={"Authorization": "Bearer"})
+    response = client.post("/v1/compras/", headers={"Authorization": "Bearer"})
     assert response.json().get("status") == 403
